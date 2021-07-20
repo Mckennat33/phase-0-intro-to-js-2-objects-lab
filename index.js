5abaf99d5d8db87f8209008ab73965fefeac2ab4
thomasmckenna <thomasmckenna12@gmail.com>
2021-07-20 16:49:58 -0400

    phase-0-intro-to-js-2-objects-lab

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-// Write your solution in this file!
+
+// function updateEmployeeAndValue(employee, key, value) {
+//    return Object.assign({}, employee, { [key]: value }); 
+// }
+let employee = {
+
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) { 
+  return Object.assign({}, employee, {[key]: value})
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) { 
+  return Object.assign(employee, {[key]: value})
+
+  //or you can do it this way.
+
+  //employee[key] = value 
+  //return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+   const newObject = Object.assign({}, employee)
+   delete newObject[key]
+   return newObject
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+   delete employee[key]
+   //const newObject = ({}key)
+   //delete newObject[employee] 
+   return employee
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
